750165c51695cd2efbf1e4c4aaf68398eec1f9b6
fagnergomes777 <phagner@outlook.com>
2025-04-18 14:44:36 +0000

    primeiro commit

--- index.js
+++ index.js
@@ -1,15 +1,51 @@
-import AlunoView from "./src/modules/aluno/views/index.js";
-import prompt from 'prompt-sync';
-const input = prompt();
+import AlunoController from "../controllers/index.js";
+import prompt from 'prompt-sync'
 
-const nome = input(`Digite o nome do aluno: `);
-const email = input(`Digite o email do aluno: `);
-const matricula = input(`Digite a matricula do aluno: `);
-const telefone = input(`Digite o telefone do aluno: `);
-const cod_turma = input(`Digite o codigo da turma: `);
-// AlunoView.criar(nome, email, matricula, telefone, cod_turma)
-// AlunoView.listarTodos();
-AlunoView.editarAluno(nome, email, matricula, telefone, cod_turma);
-AlunoView.deletarAluno(email);
-AlunoView.totalAlunos();
-AlunoView.deletarTodos();
+const input = prompt()
+class AlunoView {
+  static async criar() {
+    const nome = input("Digite o nome do aluno: ");
+    const email = input("Digite o email do aluno: ");
+    const matricula = input("Digite a matrícula do aluno: ");
+    const telefone = input("Digite o telefone do aluno: ");
+    const cod_turma = input("Digite o código da turma: ");
+    const aluno = await AlunoController.criar(
+      nome,
+      email,
+      matricula,
+      telefone,
+      cod_turma
+    );
+    console.table(aluno);
+  }
+  static async editarAluno(nome, email, matricula, telefone, cod_turma) {
+    const aluno = await AlunoController.editar(
+      nome,
+      email,
+      matricula,
+      telefone,
+      cod_turma
+    );
+    console.table(aluno);
+  }
+  static async listarTodos() {
+    const alunos = await AlunoController.listarTodos();
+    console.table(alunos);
+  }
+  static async listarPorEmail(email) {
+    const aluno = await AlunoController.listarPorEmail(email);
+    console.table(aluno);
+  }
+  static async deletarTodos() {
+    await AlunoController.deletarTodos();
+  }
+  static async deletarAluno(email) {
+    await AlunoController.deletarAluno(email);
+  }
+  static async totalAlunos() {
+    const total = await AlunoController.totalAlunos();
+    console.table(total);
+  }
+}
+
+export default AlunoView;
